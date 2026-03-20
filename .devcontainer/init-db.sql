1e9dd43c9ef3aa6a8e17eb914cad63c39d88ec1e
Subham Chakraborty <subhamchakraborty81@gmail.com>
2026-03-20 10:20:13 +0530

    Removed unused files

--- .devcontainer/init-db.sql
+++
@@ -1,6 +0,0 @@
--- Enable pgvector extension
-CREATE EXTENSION IF NOT EXISTS vector;
-
--- Enable full-text search utilities
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE EXTENSION IF NOT EXISTS unaccent;
